90b8a0feeed2351a81d6fcd1e30feb3143cf99e2
Imtious-coder <iarafat386@gmail.com>
2021-09-09 10:46:02 +0600

    Bootstrap React Router Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from './Components/Home/Home/Home';
 
 function App() {
   return (
-    <div></div>
+    
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Nav from '../Nav/Nav';
 
 const Home = () => {
     return (
-        <div>
-            
-        </div>
+        <section>
+            <Nav />
+        </section>
     );
 };
 
